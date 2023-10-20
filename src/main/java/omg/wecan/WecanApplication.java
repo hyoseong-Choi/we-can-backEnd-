20a59570241f8aec31371d73353f214a3ca3cfde
youjae <youjae0803@gmail.com>
2023-10-20 19:48:24 +0900

    project initial setting

---
+++ src/main/java/omg/wecan/WecanApplication.java
@@ -0,0 +1,13 @@
+package omg.wecan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WecanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/omg/wecan/WecanApplicationTests.java
@@ -0,0 +1,13 @@
+package omg.wecan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WecanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
