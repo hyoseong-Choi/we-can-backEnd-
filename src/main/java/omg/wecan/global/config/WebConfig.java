b72e1798fe0df6605d4fd0663ee8f91526224924
youjae <youjae0803@gmail.com>
2023-11-10 11:22:19 +0900

    feat: mapping cors origin from application.properties

--- src/main/java/omg/wecan/global/config/WebConfig.java
+++ src/main/java/omg/wecan/global/config/WebConfig.java
@@ -2,20 +2,28 @@ package omg.wecan.global.config;
 
 import lombok.RequiredArgsConstructor;
 import omg.wecan.infrastructure.oauth.basic.domain.OauthServerTypeConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+import java.util.List;
 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final List<String> corsOrigins;
+
+    public WebConfig(@Value("${cors.allow-origin.urls}") final List<String> corsOrigins){
+        this.corsOrigins = corsOrigins;
+    }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] corsOriginPattern = corsOrigins.stream().toArray(String[]::new);
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(corsOriginPattern)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
