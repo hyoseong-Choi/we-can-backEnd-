c5ae012c9958c67e086b36b3e9abe9820efa3c5f
DESKTOP-7MNK8JV\HP <gytjd9516@naver.com>
2023-11-18 19:29:30 +0900

    모집글 상세조회 댓글

--- src/main/java/omg/wecan/user/controller/UserController.java
+++ src/main/java/omg/wecan/user/controller/UserController.java
@@ -3,6 +3,7 @@ package omg.wecan.user.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import omg.wecan.user.dto.CertificationMailOutput;
+import omg.wecan.user.dto.EmailCertificationInput;
 import omg.wecan.user.dto.NewPasswordInput;
 import omg.wecan.user.dto.UserCertificationInput;
 import omg.wecan.user.service.UserFindPasswordService;
@@ -18,13 +19,18 @@ public class UserController {
         return userFindPasswordService.certifyUser(userCertificationInput);
     }
     
-    @GetMapping("/user/certification")
-    public CertificationMailOutput sendCertificationNum() {
-        CertificationMailOutput certificationMailOutput = userFindPasswordService.createMail();
+    @PostMapping("/user/certification-mail")
+    public CertificationMailOutput sendCertificationNum(@Valid @RequestBody EmailCertificationInput emailCertificationInput) {
+        CertificationMailOutput certificationMailOutput = userFindPasswordService.createMail(emailCertificationInput);
         userFindPasswordService.mailSend(certificationMailOutput);
         return certificationMailOutput;
     }
-    
+
+    @PostMapping("/user/certification-num")
+    public String enterCertificationNum(@Valid @RequestBody String certificationNum) {
+        return userFindPasswordService.validateCertificationNum(certificationNum);
+    }
+
     @PatchMapping("/user/password")
     public NewPasswordInput changePassword(@Valid @RequestBody NewPasswordInput newPasswordInput) {
         //토큰으로 유저 인증하고 레포에서 유저 이메일 가져와야함(서비스로 옮길것)
