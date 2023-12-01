ec4548ad60b45d7834db571f0070ce3288b86b45
youjae <youjae0803@gmail.com>
2023-12-01 22:41:48 +0900

    feat: @RequestBody 예외 처리 핸들러

--- src/main/java/omg/wecan/exception/customException/GlobalExceptionHandler.java
+++ src/main/java/omg/wecan/exception/customException/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import omg.wecan.jwt.exception.InvalidTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -49,4 +50,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
     }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleTokenException( HttpMessageNotReadableException e){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("400")
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+
 }
