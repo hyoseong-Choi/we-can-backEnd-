0b7cf7ce5172878b263e304ade6765b8c3d59866
youjae <youjae0803@gmail.com>
2023-12-01 22:05:03 +0900

    feat: jwt없을 때 발생하는 예외를 처리하는 핸들러

--- src/main/java/omg/wecan/exception/customException/GlobalExceptionHandler.java
+++ src/main/java/omg/wecan/exception/customException/GlobalExceptionHandler.java
@@ -1,6 +1,11 @@
 package omg.wecan.exception.customException;
 
+import omg.wecan.auth.exception.EmptyAuthorizationHeaderException;
+import omg.wecan.jwt.exception.InvalidTokenException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +18,35 @@ public class GlobalExceptionHandler {
                 .message(e.getMessage())
                 .build();
         return ResponseEntity.status(e.getCode().getStatus())
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidException( MethodArgumentNotValidException e){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("400")
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(e.getStatusCode())
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(value = EmptyAuthorizationHeaderException.class)
+    public ResponseEntity<ErrorResponse> handleTokenException( EmptyAuthorizationHeaderException e){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("400")
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+    @ExceptionHandler(value = InvalidTokenException.class)
+    public ResponseEntity<ErrorResponse> handleTokenException( InvalidTokenException e){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code("400")
+                .message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
     }
 }
