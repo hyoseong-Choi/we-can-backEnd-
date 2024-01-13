e060b6e0251e22501a385c93b2c337b4d11e53b4
Choi hyoseong <105284587+hyoseong-Choi@users.noreply.github.com>
2024-01-13 15:42:06 +0900

    Update WebConfig.java

--- src/main/java/omg/wecan/global/config/WebConfig.java
+++ src/main/java/omg/wecan/global/config/WebConfig.java
@@ -22,22 +22,22 @@ public class WebConfig implements WebMvcConfigurer {
         this.authenticationPrincipalArgumentResolver = authenticationPrincipalArgumentResolver;
     }
   
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        String[] corsOriginPattern = corsOrigins.stream().toArray(String[]::new);
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     String[] corsOriginPattern = corsOrigins.stream().toArray(String[]::new);
 
-        registry.addMapping("/**")
-                .allowedOriginPatterns(corsOriginPattern)
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.PATCH.name()
-                )
-                .allowCredentials(true)
-                .exposedHeaders("*");
-    }
+    //     registry.addMapping("/**")
+    //             .allowedOriginPatterns(corsOriginPattern)
+    //             .allowedMethods(
+    //                     HttpMethod.GET.name(),
+    //                     HttpMethod.POST.name(),
+    //                     HttpMethod.PUT.name(),
+    //                     HttpMethod.DELETE.name(),
+    //                     HttpMethod.PATCH.name()
+    //             )
+    //             .allowCredentials(true)
+    //             .exposedHeaders("*");
+    // }
 
 
     @Override
