23f4b03f2ea1815e26a5dd8635dc8a064d61380e
youjae <youjae0803@gmail.com>
2024-01-17 18:54:56 +0900

    fix: add userID to response

--- src/main/java/omg/wecan/auth/dto/authResponse/AuthResponse.java
+++ src/main/java/omg/wecan/auth/dto/authResponse/AuthResponse.java
@@ -8,9 +8,11 @@ import omg.wecan.user.entity.User;
 public class AuthResponse {
     private final AuthToken authToken;
     private final String nickName;
+    private final Long userId;
 
     public AuthResponse(AuthToken authToken, User user) {
         this.authToken = authToken;
         this.nickName = user.getNickName();
+        this.userId = user.getUserId();
     }
 }
