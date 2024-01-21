3bb550a5f368ce80bf6ffbdbb4b1ff92ace2c49c
lujae <62204810+LujaeDev@users.noreply.github.com>
2024-01-21 19:37:16 +0900

    Update OauthResponse.java

--- src/main/java/omg/wecan/infrastructure/oauth/basic/dto/response/OauthResponse.java
+++ src/main/java/omg/wecan/infrastructure/oauth/basic/dto/response/OauthResponse.java
@@ -2,12 +2,17 @@ package omg.wecan.infrastructure.oauth.basic.dto.response;
 
 import lombok.Getter;
 import omg.wecan.jwt.domain.AuthToken;
+import omg.wecan.user.entity.User;
 
 @Getter
 public class OauthResponse {
-    private AuthToken authToken;
+    private final AuthToken authToken;
+    private final String nickName;
+    private final Long userId;
 
-    public OauthResponse(AuthToken authToken) {
+    public OauthResponse(AuthToken authToken, User user) {
         this.authToken = authToken;
+        this.nickName = user.getNickName();
+        this.userId = user.getUserId();
     }
 }
