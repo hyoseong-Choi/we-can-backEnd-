4be0285860a6ce80ac64fee8723e1b38b8c7eccc
sujin11-kim <su091876@gmail.com>
2024-01-23 06:13:38 +0900

    Feat: 어제 인증 안 한 user의 failNum +1

--- src/main/java/omg/wecan/challenge/repository/UserChallengeRepository.java
+++ src/main/java/omg/wecan/challenge/repository/UserChallengeRepository.java
@@ -1,5 +1,6 @@
 package omg.wecan.challenge.repository;
 
+import omg.wecan.challenge.Enum.ChallengeStateType;
 import omg.wecan.challenge.entity.Challenge;
 import omg.wecan.challenge.entity.UserChallenge;
 import omg.wecan.user.entity.User;
@@ -18,4 +19,6 @@ public interface UserChallengeRepository extends JpaRepository<UserChallenge, Lo
     Optional<UserChallenge> findByChallengeIdAndUser(Long challengeId, User user);
 
     UserChallenge findByUserAndChallengeId(User user, Long challengeId);
+
+    List<UserChallenge> findByChallenge_State(ChallengeStateType challengeStateType);
 }
