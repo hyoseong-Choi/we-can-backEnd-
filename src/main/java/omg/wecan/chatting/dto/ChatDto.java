005afdbd706866c2a5212bc7379126eef5f1d34a
Sujin Kim <105473383+sujin11-kim@users.noreply.github.com>
2024-01-27 01:04:36 +0900

    fix: 채팅 시간 형식 변환

--- src/main/java/omg/wecan/chatting/dto/ChatDto.java
+++ src/main/java/omg/wecan/chatting/dto/ChatDto.java
@@ -18,7 +18,7 @@ public class ChatDto {
     private Long userId; // 채팅 보낸 사람
     private String nickName; // 채팅 보낸 사람 이름
     private String message; // 메시지
-    private String time; // 채팅 발송 시간
+    private LocalDateTime time; // 채팅 발송 시간
 
     public static ChatDto create(Chatting chatting) {
         ChatDto chatDto = new ChatDto();
@@ -27,9 +27,7 @@ public class ChatDto {
         chatDto.userId = chatting.getChattingRoomUser().getUser().getUserId();
         chatDto.nickName = chatting.getChattingRoomUser().getUser().getNickName();
         chatDto.message = chatting.getMessage();
-        chatDto.time = chatting.getCreatedAt().format(
-                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-        );
+        chatDto.time = chatting.getCreatedAt();
 
         return chatDto;
     }
